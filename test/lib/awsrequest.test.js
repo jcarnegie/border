adbc06162eba7a047d761bf91681d0152f321f45
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-10-29 22:20:34 -0700

    awsrequest stubs for testing.

--- test/lib/awsrequest.test.js
+++ test/lib/awsrequest.test.js
@@ -12,26 +12,34 @@ let stubbedRequest = (responseText, headers, error) => {
                 let res = null;
                 res = {
                     headers: headers || {},
-                    
+
                     setEncoding(encoding) {
                         return encoding;
                     },
 
                     on: (event, done) => {
+                        if (error) return null;
                         switch (event) {
                         case "data":
                             return done(responseText);
                         case "end":
                             return done();
-                        case "error":
-                            if (error) done(error);
-                            break;
                         default:
                             return null;
                         }
                     }
                 };
                 cb(res);
+
+                return {
+                    on: (event, done) => {
+                        if (event === "error" && error) {
+                            return done(error);
+                        }
+                    },
+
+                    end: () => {}
+                };
             }
         }
     });
@@ -45,7 +53,39 @@ describe ("awsrequest", () => {
             expect(res).to.eql("blah");
         }));
 
-        it ("should return json for application/json get request", () => {
-        });
+        it ("should convert get request application/json response to JSON", asyncTest(async () => {
+            let response = '{"status": "ok"}';
+            let headers = { "content-type": "application/json" };
+            let awsreq = stubbedRequest(response, headers);
+            let res = await awsreq.request({ method: "GET" });
+            expect(res).to.eql({ status: "ok" });
+        }));
+
+        it ("should make a post request", asyncTest(async () => {
+            let awsreq = stubbedRequest("haha");
+            let res = await awsreq.request({
+                method: "POST",
+                body: { foo: "bar" }
+            });
+            expect(res).to.eql("haha");
+        }));
+
+        it ("should convert post request application/json response to JSON", asyncTest(async () => {
+            let response = '{"status": "ok"}';
+            let headers = { "content-type": "application/json" };
+            let awsreq = stubbedRequest(response, headers);
+            let res = await awsreq.request({ method: "POST" });
+            expect(res).to.eql({ status: "ok" });
+        }));
+
+        it ("should throw an error on error", asyncTest(async () => {
+            let err = new Error("bad request");
+            try {
+                let awsreq = stubbedRequest(null, null, err);
+                await awsreq.request({ method: "GET" });
+            } catch (e) {
+                expect(e).to.eql(err);
+            }
+        }));
     });
 });
