fcf7bcbace39006f99d053137cf6bb5de6035485
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-11-15 21:33:09 -0800

    refactored to create parent resources as needed and to not create resources that already exist

--- test/lib/deploy.test.js
+++ test/lib/deploy.test.js
@@ -1,6 +1,6 @@
 import "babel/polyfill";
 import proxyquire from "proxyquire";
-// import gateway from "../../lib/apigateway"
+import asyncTest from "../asynctest";
 import Bluebird from "bluebird";
 import sinon from "sinon";
 import chai from "chai";
@@ -24,12 +24,88 @@ describe("Deploy", () => {
         spec = loadFixture("swagger-single-method");
     });
 
-    it ("should create a new API", async function() {
-        this.timeout(60000);
+    it ("should deploy a new API", async function() {
+        let accountId        = "1234567890";
+        let transpileStub    = sinon.stub();
+        let npmInstallStub   = sinon.stub();
+        let restapisStub     = sinon.stub().resolves([]);
+        let createApiStub    = sinon.stub().resolves(loadFixture("restapis-post"));
+        let resourcesStub    = sinon.stub();
+        let createResStub    = sinon.stub().resolves(loadFixture("resources-post"));
+        let updateMethodStub = sinon.stub().resolves(loadFixture("methods-put"));
+        let zipStub          = sinon.stub().resolves("foo");
+        let getUserStub      = sinon.stub().yields(null, { User: { Arn: `arn:aws:iam::${accountId}:root` } });
+        let createFuncStub   = sinon.stub().yields(null, {});
+        let addPermStub      = sinon.stub().yields(null, {});
+        let createIntegrationStub = sinon.stub().resolves({});
+        let deployStub       = sinon.stub().resolves({});
+        let deploy = proxyquire("../../lib/deploy", {
+            "./transpile": transpileStub,
+            "./npminstall": npmInstallStub,
+            "../lib/apigateway": {
+                restapis: restapisStub,
+                createRestapi: createApiStub,
+                resources: resourcesStub,
+                createResource: createResStub,
+                updateMethod: updateMethodStub,
+                createIntegration: createIntegrationStub,
+                deploy: deployStub
+            },
+            "aws-sdk": {
+                Lambda: function() {
+                    return {
+                        createFunction: createFuncStub,
+                        addPermission: addPermStub
+                    }
+                },
+                IAM: function() {
+                    return { getUser: getUserStub }
+                }
+            },
+            "./lambdazip": zipStub
+        });
+
+        resourcesStub.onCall(0).resolves([loadFixture("resources-empty-get")._embedded.item]);
+        resourcesStub.onCall(1).resolves(loadFixture("resources-multiple-get")._embedded.item);
+
+        let res = await deploy.go("us-west-2", "test", "v1", spec, "dist");
+
+        let lambdaCreateArgs = {
+            Code: { ZipFile: "foo" },
+            FunctionName: "api-test-v1-hello-get",
+            Handler: "handler",
+            Role: `arn:aws:iam::${accountId}:role/lambda_basic_execution`,
+            Runtime: "nodejs"
+        };
+
+        let lambdaAddPermArgs = {
+            Action: "lambda:InvokeFunction",
+            FunctionName: "api-test-v1-hello-get",
+            Principal: "apigateway.amazonaws.com",
+            StatementId: "api-test-v1-hello-get",
+            SourceArn: "arn:aws:apigateway:us-west-2::3e5141:/hello"
+        };
+
+        expect(transpileStub.withArgs("src/v1", "dist/v1").calledOnce).to.eql(true);
+        expect(npmInstallStub.withArgs("dist/v1/hello/get").calledOnce).to.eql(true);
+        expect(createApiStub.withArgs("us-west-2", "api-test", "api").calledOnce).to.eql(true);
+        expect(resourcesStub.withArgs("us-west-2", "cd14zqypi2").calledTwice).to.eql(true);
+        expect(createResStub.withArgs("us-west-2", "cd14zqypi2", "klqt924rw3", "hello").calledOnce).to.eql(true);
+        expect(updateMethodStub.withArgs("us-west-2", "cd14zqypi2", "3e5141", "GET").calledOnce).to.eql(true);
+        expect(zipStub.withArgs("dist/v1/hello/get").calledOnce).to.eql(true);
+        expect(createFuncStub.withArgs(lambdaCreateArgs).calledOnce).to.eql(true);
+        expect(addPermStub.withArgs(lambdaAddPermArgs).calledOnce).to.eql(true);
+        expect(createIntegrationStub.calledOnce).to.eql(true);
+        expect(deployStub.calledOnce).to.eql(true);
+    });
+
+    xit ("should deploy a new API with multiple methods", asyncTest(async () => {
+        spec = loadFixture("swagger-multi-method");
 
         let accountId        = "1234567890";
         let transpileStub    = sinon.stub();
         let npmInstallStub   = sinon.stub();
+        let restapisStub     = sinon.stub().resolves([]);
         let createApiStub    = sinon.stub().resolves(loadFixture("restapis-post"));
         let resourcesStub    = sinon.stub().resolves([loadFixture("resources-empty-get")._embedded.item]);
         let createResStub    = sinon.stub().resolves(loadFixture("resources-post"));
@@ -44,6 +120,7 @@ describe("Deploy", () => {
             "./transpile": transpileStub,
             "./npminstall": npmInstallStub,
             "../lib/apigateway": {
+                restapis: restapisStub,
                 createRestapi: createApiStub,
                 resources: resourcesStub,
                 createResource: createResStub,
@@ -67,33 +144,19 @@ describe("Deploy", () => {
 
         let res = await deploy.go("us-west-2", "test", "v1", spec, "dist");
 
-        let lambdaCreateArgs = {
-            Code: { ZipFile: "foo" },
-            FunctionName: "api-test-v1-hello-get",
-            Handler: "handler",
-            Role: `arn:aws:iam::${accountId}:role/lambda_basic_execution`,
-            Runtime: "nodejs"
-        };
-
-        let lambdaAddPermArgs = {
-            Action: "lambda:InvokeFunction",
-            FunctionName: "api-test-v1-hello-get",
-            Principal: "apigateway.amazonaws.com",
-            StatementId: "api-test-v1-hello-get",
-            SourceArn: "arn:aws:apigateway:us-west-2::3e5141:/hello"
-        };
-
         expect(transpileStub.withArgs("src/v1", "dist/v1").calledOnce).to.eql(true);
         expect(npmInstallStub.withArgs("dist/v1/hello/get").calledOnce).to.eql(true);
+        expect(npmInstallStub.withArgs("dist/v1/hello/post").calledOnce).to.eql(true);
         expect(createApiStub.withArgs("us-west-2", "api-test", "api").calledOnce).to.eql(true);
         expect(resourcesStub.withArgs("us-west-2", "cd14zqypi2").calledOnce).to.eql(true);
+        expect(resourcesStub.calledTwice).to.eql(true);
         expect(createResStub.withArgs("us-west-2", "cd14zqypi2", "klqt924rw3", "hello").calledOnce).to.eql(true);
         expect(updateMethodStub.withArgs("us-west-2", "cd14zqypi2", "3e5141", "GET").calledOnce).to.eql(true);
         expect(zipStub.withArgs("dist/v1/hello/get").calledOnce).to.eql(true);
         expect(createFuncStub.withArgs(lambdaCreateArgs).calledOnce).to.eql(true);
         expect(addPermStub.withArgs(lambdaAddPermArgs).calledOnce).to.eql(true);
         expect(createIntegrationStub.calledOnce).to.eql(true);
-        console.log(deployStub.callCount);
         expect(deployStub.calledOnce).to.eql(true);
-    });
+    }));
+
 });
