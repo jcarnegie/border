1840696eaa54369663640becaaed3f1e062e8579
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-11-27 23:26:31 -0800

    Fixed failing zip tests

--- test/lib/lambdazip.test.js
+++ test/lib/lambdazip.test.js
@@ -2,33 +2,24 @@ import "babel/polyfill";
 import proxyquire from "proxyquire";
 import sinon from "sinon";
 import chai from "chai";
-import readdir from "recursive-readdir";
 
 let expect = chai.expect;
 
 describe("Lambda Function Zip", () => {
     it ("should return a buffer", async () => {
-        let readdirStub = sinon.stub().yields(null, ["foo", "bar"]);
-        let zipAddLocalFileStub = sinon.stub();
+        let zipAddLocalFolderStub = sinon.stub();
         let zipBufferStub = sinon.stub().returns(new Buffer("lambdazip"));
-
         let lambdazip = proxyquire("../../lib/lambdazip", {
-            "recursive-readdir": readdirStub,
             "adm-zip": function() {
-                this.addLocalFile = zipAddLocalFileStub;
+                this.addLocalFolder = zipAddLocalFolderStub;
                 this.toBuffer = zipBufferStub;
             }
         });
-
         let funcDir = "/some/lambda/func/dir";
         let modDirs = `${funcDir}/node_modules`;
         let buffer = await lambdazip(funcDir);
 
-        expect(readdirStub.withArgs(modDirs).calledOnce).to.eql(true);
-        expect(zipAddLocalFileStub.getCall(0).args[0]).to.eql(`${funcDir}/index.js`);
-        expect(zipAddLocalFileStub.getCall(1).args[0]).to.eql(`${funcDir}/package.json`);
-        expect(zipAddLocalFileStub.getCall(2).args[0]).to.eql("foo");
-        expect(zipAddLocalFileStub.getCall(3).args[0]).to.eql("bar");
+        expect(zipAddLocalFolderStub.getCall(0).args[0]).to.eql(funcDir);
         expect(buffer.toString()).eql("lambdazip");
     });
 });
