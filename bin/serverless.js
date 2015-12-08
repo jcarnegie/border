f9fea92edb61c76056c8728024f312a39d747670
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-12-08 14:38:24 -0800

    Upgraded to Babel 6

--- bin/serverless.js
+++ bin/serverless.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 
-var _this = this;
+require("babel-polyfill");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _tools = require("../lib/tools");
 
-require("babel/polyfill");
-
-var _libTools = require("../lib/tools");
-
-var _libTools2 = _interopRequireDefault(_libTools);
+var _tools2 = _interopRequireDefault(_tools);
 
 require("colors");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } step("next"); }); }; }
 
 var region = process.env.AWS_DEFAULT_REGION;
 var env = process.env.NODE_ENV || "development";
@@ -36,29 +36,39 @@ var logger = function logger(level, msg) {
     console.log("[" + color(level) + "] " + msg);
 };
 
-var main = function main() {
-    return regeneratorRuntime.async(function main$(context$1$0) {
-        while (1) switch (context$1$0.prev = context$1$0.next) {
-            case 0:
-                context$1$0.prev = 0;
-                context$1$0.next = 3;
-                return regeneratorRuntime.awrap(_libTools2["default"].deploy(logger, region, env, stage, "dist"));
+var main = (function () {
+    var _this = this;
 
-            case 3:
-                context$1$0.next = 8;
-                break;
+    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.prev = 0;
+                        _context.next = 3;
+                        return _tools2.default.deploy(logger, region, env, stage, "dist");
 
-            case 5:
-                context$1$0.prev = 5;
-                context$1$0.t0 = context$1$0["catch"](0);
+                    case 3:
+                        _context.next = 8;
+                        break;
 
-                console.error(context$1$0.t0);
+                    case 5:
+                        _context.prev = 5;
+                        _context.t0 = _context["catch"](0);
 
-            case 8:
-            case "end":
-                return context$1$0.stop();
-        }
-    }, null, _this, [[0, 5]]);
-};
+                        console.error(_context.t0);
+
+                    case 8:
+                    case "end":
+                        return _context.stop();
+                }
+            }
+        }, _callee, _this, [[0, 5]]);
+    }));
+
+    return function main() {
+        return ref.apply(this, arguments);
+    };
+})();
 
 main();
